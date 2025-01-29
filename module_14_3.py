ddb9ef768e3c8cd86125b1f31bc8b26590e14942
123e456y78 <137395377+123e456y78@users.noreply.github.com>
2025-01-29 08:28:59 +0300

    Update module_14_3.py

--- module_14_3.py
+++ module_14_3.py
@@ -8,7 +8,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import FSInputFile
 import asyncio
-api = '7314899914:AAHcsc1SyUJccc3bK3jNxYvAkCdAfC8fxas'
+api = '!!!!!!!!!'
 bot = Bot(token=api)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
